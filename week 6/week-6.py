5c3b728835a7f9b2997c75188a064a0848230284
Mohammed <mohammedoyediran@hotmail.com>
2025-04-29 14:55:03 +0400

    week 7

---
+++ week 6/week-6.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Sample data
+activities = ['Sleeping', 'Eating', 'Coding', 'Gaming']
+hours = [8, 2, 8, 6]
+
+plt.pie(hours, labels=activities, autopct='%1.2f%%')
+plt.title("Daily Activities")
+plt.show()
+
+# Example: showing frequency of values
+ages = [22, 21, 20, 23, 24, 22, 20, 21, 22, 25, 23]
+
+plt.hist(ages, bins=5, color='purple')
+plt.title("Age Distribution")
+plt.xlabel("Age")
+plt.ylabel("Frequency")
+plt.show()
